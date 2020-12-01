934328caa5a11c3a80ef64314c815a6969cc7a28
maha-jwa <68131647+maha-jwa@users.noreply.github.com>
2020-12-01 17:03:30 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     // register service worker
-    navigator.serviceWorker.register('../service-worker.js');
+    navigator.serviceWorker.register('service-worker.js');
 }
 
 
