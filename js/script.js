16f04a4c5cf9c456f516c8d745fb2dce1e65279f
maha-jwa <68131647+maha-jwa@users.noreply.github.com>
2020-12-01 18:17:12 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     // register service worker
-    navigator.serviceWorker.register('/service-worker.js', {scope: './'}).then(function(registration) {
+    navigator.serviceWorker.register('./service-worker.js', {scope: './'}).then(function(registration) {
     console.log('Service worker registration succeeded:', registration);
   }, /*catch*/ function(error) {
     console.log('Service worker registration failed:', error);
